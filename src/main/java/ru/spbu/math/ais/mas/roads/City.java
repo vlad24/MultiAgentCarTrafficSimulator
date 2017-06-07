cf3cd5dc43732e70238b340186db7754e06413c5
PolinaDurdeva <polina.durdeva@yandex.ru>
2017-06-07 21:18:05 +0300

    add Dijkstra's algorithm

--- src/main/java/ru/spbu/math/ais/mas/roads/City.java
+++ src/main/java/ru/spbu/math/ais/mas/roads/City.java
@@ -1,19 +1,31 @@
 package ru.spbu.math.ais.mas.roads;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import jade.core.Agent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ru.spbu.math.ais.mas.roads.wrappers.Graph;
+
 public class City extends Agent {
 	private static final long serialVersionUID = 1L;
-	
+	private Graph cityGraph;
 
 	private static final Logger log = LoggerFactory.getLogger(City.class);
 	
 	@Override
 	protected void setup() {
 		log.info("{} is ready. Got args:{}", getLocalName(), getArguments());
+		String carsFilename  = String.valueOf(getArguments()[0]);
+		String roadsFilename = String.valueOf(getArguments()[1]);
+		Path filePath = Paths.get("src", "main", "resources", roadsFilename);
+		cityGraph = new Graph(filePath.toFile());
+		log.info("Got graph: {}", cityGraph);
+		log.info("minDistances {}", cityGraph.getMinDistances(1));
 	}
 
 }
--- src/main/java/ru/spbu/math/ais/mas/roads/wrappers/Graph.java
+++ src/main/java/ru/spbu/math/ais/mas/roads/wrappers/Graph.java
@@ -1,14 +1,26 @@
 package ru.spbu.math.ais.mas.roads.wrappers;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * @author vlad
+ * @author vlad and polina
  * Graph implementation.
  * Graph stores all the data in adjacency matrix form. For simplicity
  */
 public class Graph {
+	private static final String SEPARATOR = "\t";
+	private static final String COMMENT = "#";
+	
+	private static final Logger log = LoggerFactory.getLogger(Graph.class);
 
 	private ArrayList<ArrayList<Integer>> adjMatrix;
 	
@@ -17,6 +29,67 @@ public class Graph {
 	}
 	
 	public Graph(File file) {
-		//TODO implement
+		adjMatrix = new ArrayList<ArrayList<Integer>>();
+		try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+			String currentLine;
+			while ((currentLine = reader.readLine()) != null) {
+				if (!currentLine.startsWith(COMMENT) && !currentLine.isEmpty()){
+					String[] elements =  currentLine.split(SEPARATOR);
+					ArrayList<Integer> matrixLine = new ArrayList<Integer>();
+					for (String el : elements) {
+						matrixLine.add(Integer.parseInt(el));
+					}
+					adjMatrix.add(matrixLine);
+				}
+			}
+		}catch (IOException readerExceprion) {
+			readerExceprion.printStackTrace();
+		}catch (NumberFormatException wrongNumberException) {
+			log.error("Wrong number {}", wrongNumberException);
+		}
 	}
+
+	@Override
+	public String toString() {
+		return "Graph [adjMatrix=" + adjMatrix + "]";
+	}
+	public int[] getMinDistances(int source){
+		int countRoads = adjMatrix.size();
+		boolean[] visited = new boolean[countRoads];
+		int[] distances = new int[countRoads];
+		for (int i = 0; i < countRoads; i++) {
+			visited[i] = false;
+			distances[i] = Integer.MAX_VALUE;
+		}
+		visited[source] = true;
+		distances[source] = 0;
+		int minVertex = source;
+		int countVisited = 1;
+		while (countVisited != countRoads){
+			int minDistance = Integer.MAX_VALUE;
+			int index = -1;
+			for (int i = 0; i < countRoads; i++) {
+				if (areConnected(minVertex, i) && !visited[i] ){
+					distances[i] = Math.min(distances[i], distances[minVertex] + getEdgeLength(minVertex, i));
+					if (minDistance > distances[i]){
+						minDistance = distances[i];
+						index = i;
+					}
+				}					
+			}
+			visited[minVertex] = true;
+			countVisited++;
+			minVertex = index;
+		}
+		return distances;
+	}
+
+	private Integer getEdgeLength(int i, int j) {
+		return adjMatrix.get(i).get(j);
+	}
+
+	private boolean areConnected(int i, int j) {
+		return getEdgeLength(i, j) != 0;
+	}
+	
 }
