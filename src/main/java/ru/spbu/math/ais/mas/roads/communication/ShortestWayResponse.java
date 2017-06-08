a14e2d43514f76dcf02b7486fcb78e2fb0075995
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-06-08 20:50:22 +0300

    response object introduced

---
+++ src/main/java/ru/spbu/math/ais/mas/roads/communication/ShortestWayResponse.java
@@ -0,0 +1,24 @@
+package ru.spbu.math.ais.mas.roads.communication;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class ShortestWayResponse implements Serializable{
+	
+	private Map<String, Object> wayInfo;
+
+	public ShortestWayResponse(Map<String, Object> wayInfo) {
+		super();
+		this.wayInfo = wayInfo;
+	}
+
+	public Map<String, Object> getWayInfo() {
+		return wayInfo;
+	}
+
+	public void setWayInfo(Map<String, Object> wayInfo) {
+		this.wayInfo = wayInfo;
+	}
+
+}
