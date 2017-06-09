120f4b1a00b30b059f6b6800a70b7049e41e4385
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-06-09 17:34:58 +0300

    city is configured from outside with a provided name. It acts as a server and registers/inregisters cars

--- src/main/java/ru/spbu/math/ais/mas/roads/city/City.java
+++ src/main/java/ru/spbu/math/ais/mas/roads/city/City.java
@@ -1,27 +1,22 @@
-package ru.spbu.math.ais.mas.roads;
+package ru.spbu.math.ais.mas.roads.city;
 
-import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.lang.acl.ACLMessage;
-import jade.wrapper.AgentController;
-import jade.wrapper.ControllerException;
-import jade.wrapper.PlatformController;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ru.spbu.math.ais.mas.roads.cars.Car;
-import ru.spbu.math.ais.mas.roads.cars.DrivingStrategy;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 import ru.spbu.math.ais.mas.roads.wrappers.Graph;
 import ru.spbu.math.ais.mas.roads.wrappers.communication.RoadsUpdateRequest;
 import ru.spbu.math.ais.mas.roads.wrappers.communication.ShortestWayRequest;
 import ru.spbu.math.ais.mas.roads.wrappers.communication.ShortestWayResponse;
+import ru.spbu.math.ais.mas.roads.wrappers.communication.TripFinishReport;
+import ru.spbu.math.ais.mas.roads.wrappers.communication.TripStartRequest;
+import ru.spbu.math.ais.mas.roads.wrappers.communication.TripStartResponse;
 
 public class City extends Agent {
 	private static final long serialVersionUID = 1L;
@@ -29,55 +24,23 @@ public class City extends Agent {
 	
 	public static final String SHORTEST_WAY_CONVERSATION = "shortestWayConversation";
 	public static final String ROADS_UPDATE_CONVERSATION  = "roadUpdateConversation";
-	
-	private Parser entityParser;
+	public static final String START_TRIP_CONVERSATION = "registerTrip";
+	public static final String FINISH_TRIP_CONVERSATION = "carStat";
 	private Graph cityGraph;
+	
 	
 	@Override
 	protected void setup() {
 		log.info("{} is ready. Got args:{}", getLocalName(), getArguments());
-		Path carsFilePath  = Paths.get("src", "main", "resources", String.valueOf(getArguments()[0]));
-		Path roadsFilePath = Paths.get("src", "main", "resources", String.valueOf(getArguments()[1]));
-		entityParser = new Parser();
-		setupRoads(roadsFilePath.toFile());
-		setupCars(carsFilePath.toFile());
-		addBehaviour(new MonitoringBehaviour(this));
-		//log.debug("Min distances {}", cityGraph.getMinDistances(1,7));
+		this.cityGraph = (Graph) getArguments()[0];
+		addBehaviour(new ServerBehaviour(this));
+		log.trace("Min distances {}", cityGraph.getMinDistances(1,7));
 	}
-	
-	private void setupRoads(File fileWithRoads) {
-		cityGraph = entityParser.parseGraph(fileWithRoads);
-		log.debug("Got graph: {}", cityGraph);
-	}
-	
-	private void setupCars(File fileWithCars) {
-		PlatformController container = getContainerController();
-		Map<String, Object> carConfig = this.entityParser.parseCarFile(fileWithCars);
-		for (ArrayList<String> carParts: (ArrayList<ArrayList<String>>)carConfig.get(Parser.CAR_ITEMS_KEY)) {
-			String carName = carParts.get(0);
-			String carSrc  = carParts.get(1);
-			String carDst  = carParts.get(2);
-			try {
-				AgentController carController = container.createNewAgent(carName, 
-						Car.class.getCanonicalName(), 
-						new Object[] {
-							getLocalName(),
-							carSrc,
-							carDst,
-							carConfig.getOrDefault(Parser.CAR_DRIVING_STRATEGY_KEY, DrivingStrategy.DUMMY) 
-						}
-				);
-				carController.start();
-			} catch (ControllerException e) {
-				log.error("Error while creating agent: {}", e);
-			}   
-		}
-	}
-	
 	
 	@SuppressWarnings("serial")
-	private class MonitoringBehaviour extends CyclicBehaviour{
-		public MonitoringBehaviour(City city) {
+	private class ServerBehaviour extends CyclicBehaviour{
+		
+		public ServerBehaviour(City city) {
 			super(city);
 		}
 
@@ -85,21 +48,33 @@ public class City extends Agent {
 		public void action() {
 			try {
 				ACLMessage message = myAgent.blockingReceive();
-				if (SHORTEST_WAY_CONVERSATION.equalsIgnoreCase(message.getConversationId())){
-					//FIXME
+				if (START_TRIP_CONVERSATION.equalsIgnoreCase(message.getConversationId())) {
+					TripStartRequest request = (TripStartRequest)message.getContentObject();
+					//TODO monitor cars smarter
+					replyWithContent(message, new TripStartResponse(true));
+				}else if (SHORTEST_WAY_CONVERSATION.equalsIgnoreCase(message.getConversationId())){
+					//TODO check this code
 					ShortestWayRequest request = (ShortestWayRequest)message.getContentObject();
 					Map<String, Object> wayInfo = cityGraph.getMinDistances(request.getFrom(), request.getTo());
-					ShortestWayResponse response = new ShortestWayResponse(wayInfo);
-					ACLMessage reply = message.createReply();
-					reply.setContentObject(response);
-					send(reply);
+					replyWithContent(message, new ShortestWayResponse(wayInfo));
 				}else if(ROADS_UPDATE_CONVERSATION.equalsIgnoreCase(message.getConversationId())){
 					RoadsUpdateRequest request = (RoadsUpdateRequest)message.getContentObject();
-					//TODO
+					//TODO process changes
+					
+				}else if(FINISH_TRIP_CONVERSATION.equalsIgnoreCase(message.getConversationId())){
+					TripFinishReport request = (TripFinishReport)message.getContentObject();
+					//TODO process report
+					
 				}
 			} catch (Exception e) {
-				log.error("Error in big city life: {}", e);
+				log.error("Error in big city life!", e);
 			}
+		}
+
+		private void replyWithContent(ACLMessage message, Serializable response) throws IOException {
+			ACLMessage reply = message.createReply();
+			reply.setContentObject(response);
+			send(reply);
 		}
 
 	}
