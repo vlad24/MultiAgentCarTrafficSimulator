a9d301739e41595fec2471a09c01133993ce6466
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-06-14 10:35:10 +0300

    car check request filled with data

--- src/main/java/ru/spbu/math/ais/mas/citycars/wrappers/communication/CarCheckRequest.java
+++ src/main/java/ru/spbu/math/ais/mas/citycars/wrappers/communication/CarCheckRequest.java
@@ -1,7 +1,17 @@
 package ru.spbu.math.ais.mas.citycars.wrappers.communication;
 
-public class CarCheckRequest implements CityCommunicationUnit{
+import java.util.Date;
 
+import ru.spbu.math.ais.mas.citycars.wrappers.Pair;
+
+
+//TODO lombok
+public class CarCheckRequest implements CityCommunicationUnit{
+	
+	String carId;
+	Date requestTime;
+	Pair requestedRoad;
+	
 	@Override
 	public CityMessageSubject getSubject() {
 		return CityMessageSubject.CAR_CHECK;
